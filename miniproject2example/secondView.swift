ec892741068aae088b37d91bb141d92a68786a92
Megan <meganmarieesl@gmail.com>
2022-07-19 14:22:27 -0400

    Added a new button to my file!

---
+++ miniproject2example/secondView.swift
@@ -0,0 +1,30 @@
+//
+//  secondView.swift
+//  miniproject2example
+//
+//  Created by Megan Nepshinsky on 7/14/22.
+//
+
+import UIKit
+
+class secondView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("here's my second one!")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
